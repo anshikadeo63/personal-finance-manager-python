b3d5dfa3703fc5131c6c0dc3dc0f8b2ecb38902b
anshikadeo63 <anshikadeo63@gmail.com>
2026-07-24 23:35:30 -0400

    Create main application menu and connect transaction services + analytics

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
 from services.transaction_functions import *
-from models.transaction import Transaction
+from services.analytics import *
 
 transaction_main_list = []
 
-# StoreTransaction(transaction_main_list)
-# DisplayTransaction(transaction_main_list)
+while True:
+    print("\n=============================\nPersonal Finance Manager\n=============================\n1. Add Transaction\n2. Display Transaction\n3. Update Transaction\n4. Delete Transaction\n5. Calculate Balance\n6. Expense Summary\n7. Highest Expense\n8. Exit")
+    while True:
+        try:
+            selected_choice = int(input("Enter choice: "))
+            if (1<= selected_choice <=8):
+                pass
+            else:
+                print("Enter from the above choices only!")
+        except (ValueError):
+            print("Enter a number only!")
+        else:
+            break
+        
+    match (selected_choice):
+        case 1:
+            StoreTransaction(transaction_main_list)
+            enter_input = input("Press Enter to continue.......")
+        case 2:
+            DisplayTransaction(transaction_main_list)
+            enter_input = input("Press Enter to continue.......")
+        case 3:
+            UpdateTransaction(transaction_main_list)
+            enter_input = input("Press Enter to continue.......")
+        case 4:
+            DeleteTransaction(transaction_main_list)
+            enter_input = input("Press Enter to continue.......")
+        case 5:
+            total_income, total_expense, balance = CalculateBalance(transaction_main_list)
+            print(f"\n----------------------------\nTotal Income: {total_income}\nTotal Expense: {total_expense}\nBalance: {balance}\n----------------------------\n")
+            enter_input = input("Press Enter to continue.......")
+        case 6:
+            dict_expense = ExpenseSummary(transaction_main_list)
+            print(f"\n---------------Expense Summary---------------")
+            for key in dict_expense:
+                print(f"{key}: {dict_expense[key]}")
+            print(f"-----------------------------------------------")
+            enter_input = input("Press Enter to continue.......")
+        case 7:
+            return_value = HighestExpense(transaction_main_list)
+            if return_value == "No category available":
+                print(return_value)
+                enter_input = input("Press Enter to continue.......") 
+            else:
+                category, highest_expense_amount = return_value
+                print(f"\n---------------Highest expense---------------")
+                print(f"Category: {category}\nAmount: {highest_expense_amount}")
+                enter_input = input("Press Enter to continue.......")
+        case 8:
+            print("Thank you for visiting our transaction management system!")
+            break
+
+
