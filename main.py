6e034a96889ac3ace4416a69db9fa641052921cc
anshikadeo63 <anshikadeo63@gmail.com>
2026-08-22 23:28:30 -0400

    Added budget and report features in main menu

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from services.transaction_functions import *
 from services.analytics import *
 from storage.csv_handler import *
+from services.budget_management import *
 
 transaction_main_list = LoadTransactions()
+user_budget = None
 
 while True:
-    print("\n=============================\nPersonal Finance Manager\n=============================\n1. Add Transaction\n2. Display Transaction\n3. Update Transaction\n4. Delete Transaction\n5. Search Transaction\n6. Calculate Balance\n7. Expense Summary\n8. Highest Expense\n9. Exit")
+    print("\n=============================\nPersonal Finance Manager\n=============================\n1. Add Transaction\n2. Display Transaction\n3. Update Transaction\n4. Delete Transaction\n5. Search Transaction\n6. Calculate Balance\n7. Expense Summary\n8. Highest Expense\n9. Budgets\n10. Reports \n11. Exit")
     while True:
         try:
             selected_choice = int(input("Enter choice: "))
-            if (1<= selected_choice <=9):
+            if (1<= selected_choice <=11):
                 pass
             else:
                 print("Enter from the above choices only!")
@@ -56,6 +58,70 @@ while True:
                 print(f"Category: {category}\nAmount: {highest_expense_amount}")
                 enter_input = input("Press Enter to continue.......")
         case 9:
+            while True:
+                print("\n-----------------------------\n          Budget\n-----------------------------\n1. Set Budget\n2. View Budget\n3. Update Budget\n4. Delete Budget\n5. Budget Status\n6. Back to Main Menu")
+                while True:
+                    try:
+                        selected_choice_b = int(input("Enter choice: "))
+                        if (1<= selected_choice_b <=6):
+                            pass
+                        else:
+                            print("Enter from the above choices only!")
+                    except (ValueError):
+                        print("Enter a number only!")
+                    else:
+                        break
+                    
+                match (selected_choice_b):
+                    case 1:
+                        user_budget = SetBudget()
+                        enter_input = input("Press Enter to continue.......")
+                    case 2:
+                        if user_budget is None:
+                            print("No budget has been set!")
+                        else:
+                            ViewBudget(user_budget)
+                        enter_input = input("Press Enter to continue.......")
+                    case 3:
+                        user_budget = UpdateBudget()
+                        enter_input = input("Press Enter to continue.......")
+                    case 4:
+                        if user_budget is None:
+                            print("No budget has been set!")
+                        else:
+                            user_budget = DeleteBudget(user_budget)
+                        enter_input = input("Press Enter to continue.......")
+                    case 5:
+                        if user_budget is None:
+                            print("No budget has been set!")
+                        else:
+                            Budget_Status(transaction_main_list, user_budget)
+                        enter_input = input("Press Enter to continue.......")
+                    case 6:
+                        break
+        case 10:
+            while True:
+                print("\n-----------------------------\n          Reports\n-----------------------------\n1. Monthly Report\n2. Category-wise Report\n3. Back to Main Menu")
+                while True:
+                    try:
+                        selected_choice_r = int(input("Enter choice: "))
+                        if (1<= selected_choice_r <=3):
+                            pass
+                        else:
+                            print("Enter from the above choices only!")
+                    except (ValueError):
+                        print("Enter a number only!")
+                    else:
+                        break
+                    
+                match (selected_choice_r):
+                    case 1:
+                        MonthlyReports(transaction_main_list)
+                    case 2:
+                        CategoryBasedReport(transaction_main_list)
+                    case 3:
+                        break
+        case 11:
             SaveTransactionsToCSV(transaction_main_list)
             print("Thank you for visiting our transaction management system!")
             break
